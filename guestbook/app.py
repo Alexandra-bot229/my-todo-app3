d9cb6b2bc03d27a1e4980093bb90437e819ef151
Alexandra-bot229 <yakubkainna@yandex.ru>
2026-06-08 09:17:08 +0000

    ПР9: Гостевая книга - база данных и вывод сообщений

---
+++ guestbook/app.py
@@ -0,0 +1,14 @@
+from flask import Flask, render_template
+from database import init_db, get_all_messages
+
+app = Flask(__name__)
+
+init_db()
+
+@app.route('/')
+def index():
+    messages = get_all_messages()
+    return render_template('index.html', messages=messages)
+
+if __name__ == '__main__':
+    app.run(debug=True)
